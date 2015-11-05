030557e34c3439caf3bfd665d072798f90c2aba7
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-11-05 15:52:59 +0100

    eliminando tambien las imagenes gif.

--- borrar_imagenes_duplicadas.py
+++ borrar_imagenes_duplicadas.py
@@ -6,7 +6,8 @@ import reducir_dump
 def listar_imagenes_en_disco():
     for file in reducir_dump.find('./export/images'):
         if r'/2F/' in file:
-            if file.endswith('.jpg') or file.endswith('.png'):
+            nombre = file.lower()
+            if nombre.endswith('.jpg') or nombre.endswith('.png') or nombre.endswith('.gif'):
                 yield file
 
 
