358ff85fc1da9ac28741d8d149795b894c9c8529
Ignacio Losiggio <iglosiggio@gmail.com>
2016-07-19 15:02:27 -0300

    [ZOOM] Ahora se mantiene al cambiar de página

--- src/js/zoom.js
+++ src/js/zoom.js
@@ -4,5 +4,11 @@ $(function() {
 
     input.change(function() {
         win.zoomLevel = input.val();
+        global.zoomLevel = input.val();
     });
+
+    /* Seteo el zoom de acuerdo a si ya lo habían cambiado */
+    input.val(global.zoomLevel || 0);
+    /* input.val no tira el evento */
+    input.change();
 });
